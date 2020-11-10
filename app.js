f538db9a5ce54fdf0c0588c8383554153984b90d
lmachens <leon.machens@googlemail.com>
2020-11-10 12:53:39 +0100

    ✨ Ask for master password

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
+const inquirer = require("inquirer");
+
 console.log("PW4U");
 
 const args = process.argv.slice(2);
 const passwordName = args[0];
 console.log(`You want to know the password of '${passwordName}'`);
 
-if (passwordName === "caro") {
-  console.log("Password is honigblume");
-} else {
-  console.log("Unknown password");
+const secretMasterPassword = "baum";
+const questions = [
+  {
+    type: "input",
+    name: "masterPassword",
+    message: "What is the super secret master password?",
+  },
+];
+
+async function validateAccess() {
+  const { masterPassword } = await inquirer.prompt(questions);
+
+  if (masterPassword !== secretMasterPassword) {
+    console.error("You are not welcome here! 👿");
+    validateAccess();
+    return;
+  }
+
+  if (passwordName === "caro") {
+    console.log("Password is honigblume");
+  } else {
+    console.log("Unknown password");
+  }
 }
+
+validateAccess();
