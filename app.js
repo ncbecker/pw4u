77df6da3a7817a1f266029c1fc4600b4a5212a23
lmachens <leon.machens@googlemail.com>
2020-11-10 11:27:24 +0100

    Read arguments from command line

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+console.log("PW4U");
+
+const args = process.argv.slice(2);
+const passwordName = args[0];
+console.log(`You want to know the password of '${passwordName}'`);
+
+if (passwordName === "caro") {
+  console.log("Password is honigblume");
+} else {
+  console.log("Unknown password");
+}
